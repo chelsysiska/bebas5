dd4a73c36395fbc9426329933d7f21ff071c2ccb
chelsysiska <clsysiskaa@gmail.com>
2025-04-21 13:30:57 +0700

    File Baru

---
+++ laravel/database/migrations/2025_04_21_040833_create_siswas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->id();
+            $table->char('nis', 7)->unique();
+            $table->string('nama_siswa');
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->foreignId('id_kelas')->references('id')->on('kelas');
+            $table->foreignId('wali_murid_id')->references('id')->on('wali_murids'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('siswas');
+    }
+};
